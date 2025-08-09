19a1e89565cd2c574e6ff088221ecd5d16725867
klink-hub <kmjlgj44@gmail.com>
2025-08-09 17:02:14 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx'; // App.jsx 파일을 import
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App /> {/* App 컴포넌트를 렌더링 */}
+  </React.StrictMode>,
+);
